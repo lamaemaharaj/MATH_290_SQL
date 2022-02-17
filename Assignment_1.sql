df15f9a5eb174e4f5d22a41d2557288549799e7c
Lamae Maharaj <44562026+lamaemaharaj@users.noreply.github.com>
2022-02-17 16:55:39 -0500

    Add files via upload

---
+++ Assignment_1.sql
@@ -0,0 +1,19 @@
+create table
+
+math290_2.public.class_roster(
+"id" bigint
+,"name" varchar
+,"last_name" varchar
+,"passion" varchar
+,"link_to_interest" varchar 
+,"email_address" varchar 
+,"github_handle" varchar 
+,"goodreads_link" varchar 
+,"operating_system" varchar 
+,"coding_buddy_name" varchar 
+,"group_id" integer
+,constraint "id" primary key 
+("id")
+);
+
+select * from class_roster 
